99d16e79d4475bc6968a8cbff4d7da5165cdf3c6
Anshuman Rana <56511928+anshumanrana331@users.noreply.github.com>
2021-06-28 23:26:53 +0530

    Add files via upload

---
+++ assets/projectsData.js
@@ -0,0 +1,31 @@
+export const projectsData = [
+    {
+        projectName: 'StoryBooks',
+        projectDescription:
+            'Do you find it difficult to name your projects or ventures? Fear not! Check out this cool name suggestion application which will help you find the best name for your next project, venture, or startup. Keep building!',
+        imageUrl: 'https://drive.google.com/file/d/1vfWhFYDhnv1cRVNbtuzaEo7_YYj7Q1kT/view?usp=sharing',
+        projectUrl: 'http://theleanprogrammer.com/name-it/',
+    },
+    {
+        projectName: 'NameZone!',
+        projectDescription:
+            'Do you find it difficult to name your projects or ventures? Fear not! Check out this cool name suggestion application which will help you find the best name for your next project, venture, or startup. Keep building!',
+        imageUrl: 'https://madhavbahl.tech/portfolio-assets/proj1.PNG',
+        projectUrl: 'http://theleanprogrammer.com/name-it/',
+    },
+    {
+        projectName: 'TypeGraph',
+        projectDescription:
+            'Having a good typing speed will always help you as a developer! Here, I present you the typing speed test application, where you can check your typing speed (in wpm) and share the results with your friends too.',
+        imageUrl: 'https://madhavbahl.tech/portfolio-assets/proj2.PNG',
+        projectUrl: 'http://theleanprogrammer.com/flashtype/',
+    },
+    {
+        projectName: 'Portfolio Website using HTML and CSS',
+        projectDescription:
+            'As a developer/designer, you should definitely have your own portfolio website. There are endless benefits. But, a lot of people feel that it would be tough to make one. So here I bring you a tutorial to make your own website.',
+        imageUrl: 'https://madhavbahl.tech/portfolio-assets/proj5.PNG',
+        videoUrl: 'http://theleanprogrammer.com/aam',
+        projectUrl: 'http://madhavbahl.tech/portfolio/',
+    },
+];
---
+++ assets/skillsData.js
@@ -0,0 +1,42 @@
+export const skillList = [
+    {
+        skillName: 'C++',
+        skillUrl: 'https://image.flaticon.com/icons/png/512/74/74897.png',
+    },    
+    {
+        skillName: 'DSA',
+        skillUrl: 'https://github.com/anshumanrana331/Portfolio/blob/master/img/ds%20logo.png?raw=true',
+    },
+    {
+        skillName: 'HTML',
+        skillUrl: 'https://madhavbahl.tech/portfolio-assets/html.png',
+    },
+    {
+        skillName: 'CSS',
+        skillUrl: 'https://madhavbahl.tech/portfolio-assets/css.png',
+    },
+    {
+        skillName: 'JavaScript',
+        skillUrl: 'https://madhavbahl.tech/portfolio-assets/js.png',
+    },
+    {
+        skillName: 'React',
+        skillUrl: 'https://madhavbahl.tech/portfolio-assets/react.png',
+    },
+    {
+        skillName: 'NodeJS',
+        skillUrl: 'https://madhavbahl.tech/portfolio-assets/node.png',
+    },
+    {
+        skillName: 'MongoDB',
+        skillUrl: 'https://madhavbahl.tech/portfolio-assets/mongo.png',
+    },
+    {
+        skillName: 'Redux',
+        skillUrl: 'https://madhavbahl.tech/portfolio-assets/redux.png',
+    },
+    {
+        skillName: 'Java',
+        skillUrl: 'https://madhavbahl.tech/portfolio-assets/java.png',
+    },
+];
